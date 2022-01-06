eec921cdf086bdc23faf984a5228aa9b33bfe2de
zakaria444 <elismailizakariae@gmail.com>
2022-01-06 11:51:39 +0100

    install sequelize

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const expres = require('express')
-
 const app = expres()
-
-
+const db = require('./models')
+const port = process.env.port || 3000;
+app.use(expres.urlencoded({extended: true}));
+app.use(expres.json());
 app.get('/index.ejs',(req, res)=>{
     
     res.end()
